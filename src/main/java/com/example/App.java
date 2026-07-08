c6c8fb651ecad8778d94f127bd418e715da16966
Dileep93911 <lsdileeproyal@gmail.com>
2026-07-08 23:12:39 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
         server.createContext("/", exchange -> {
-            String response = "Hello yamini how r u i love u!";
+            String response = "Hello guys how are u !";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
